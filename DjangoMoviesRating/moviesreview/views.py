e5c922a33512850e49f2e4cacfac563682cf2029
Diego Orellana Ávila <dior27@duck.com>
2025-02-16 21:25:04 -0500

    format

--- DjangoMoviesRating/moviesreview/views.py
+++ DjangoMoviesRating/moviesreview/views.py
@@ -16,10 +16,12 @@ class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+
 class ReviewViewSet(ModelViewSet):
     permission_classes = [DjangoModelPermissions]
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
@@ -36,6 +38,7 @@ class CriticReviewListView(generics.ListAPIView):
     queryset = get_queryset
     serializer_class = ReviewSerializer
 
+
 class CriticReviewUpdateView(generics.UpdateAPIView):
     permission_classes = [DjangoModelPermissions]
 
@@ -45,11 +48,15 @@ class CriticReviewUpdateView(generics.UpdateAPIView):
     queryset = get_queryset
     serializer_class = ReviewSerializer
 
+
 class CriticReviewDeleteView(generics.DestroyAPIView):
     permission_classes = [DjangoModelPermissions]
+
     def get_queryset(self):
         return Review.objects.filter(user=self.request.user)
+
     queryset = get_queryset
     serializer_class = ReviewSerializer
+
 
 # endregion Reviews
