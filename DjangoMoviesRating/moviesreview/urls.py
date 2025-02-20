bc35a18a9de0fc9070877603abc1cccd9b094fb4
Diego Orellana Ávila <dior27@duck.com>
2025-02-19 20:44:51 -0500

    wip: CriticAPIView

--- DjangoMoviesRating/moviesreview/urls.py
+++ DjangoMoviesRating/moviesreview/urls.py
@@ -5,6 +5,8 @@ from .views import (
     ReviewViewSet,
     MovieViewSet,
     CriticReviewListView,
+    CriticReviewUpdateView,
+    CriticReviewDeleteView,
 )
 
 defaultRouter = DefaultRouter()
@@ -14,6 +16,8 @@ defaultRouter.register("movies", MovieViewSet)
 
 urlpatterns = [
     path('reviews/critic/', CriticReviewListView.as_view(), name="reviews-list"),
+    path('reviews/critic/<int:pk>/', CriticReviewUpdateView.as_view(), name="reviews-update"),
+    path('reviews/critic/<int:pk>/', CriticReviewDeleteView.as_view(), name="reviews-delete"),
 ]
 
 urlpatterns += defaultRouter.urls
--- DjangoMoviesRating/moviesreview/views.py
+++ DjangoMoviesRating/moviesreview/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.response import Response
 from .models import Movie, Director, Review
 from .serializers import MovieSerializer, DirectorSerializer, ReviewSerializer
 
@@ -24,6 +25,86 @@ class ReviewViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def _check_group(self, request, review):
+        """
+        Verifica si el usuario autenticado tiene permiso para acceder a la
+        rese a proporcionada.
+
+        Si el usuario es el dueño de la reseña o pertenece al grupo
+        "Movies Administrators", el método devuelve True. De lo contrario,
+        devuelve una respuesta de error 403.
+
+        :param request: La solicitud HTTP que contiene la informaci n del usuario
+        autenticado.
+        :param review: La rese a a la que se intenta acceder.
+        :return: True si el usuario tiene permiso, o una respuesta de error 403 en
+        caso contrario.
+        """
+        if review.user == request.user:
+            return True
+
+        if request.user.groups.filter(name="Movies Administrators").exists():
+            return True
+
+        return Response(
+            {"detail": "Solo los administradores pueden acceder a este endpoint."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    def update(self, request, *args, **kwargs):
+        """
+        Actualiza una reseña existente.
+
+        Este método verifica si el usuario autenticado tiene permiso para editar
+        la reseña. Si el usuario es el dueño de la reseña o pertenece al grupo
+        "Movies Administrators", el proceso de actualización continúa. De lo
+        contrario, se devuelve una respuesta de error 403.
+
+        :param request: La solicitud HTTP que contiene los datos de actualización.
+        :param args: Argumentos adicionales.
+        :param kwargs: Argumentos adicionales con nombre.
+        :return: Una respuesta HTTP con el resultado de la operación de actualización.
+        """
+
+        review = self.get_object()
+        permission_check = self._check_group(request, review)
+
+        if isinstance(permission_check, Response):
+            return permission_check  # Retorna la respuesta 403 si no tiene permisos
+
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Actualiza parcialmente una reseña existente.
+
+        Este método verifica si el usuario autenticado tiene permiso para editar
+        la reseña. Si el usuario es el due o de la reseña o pertenece al grupo
+        "Movies Administrators", el proceso de actualización parcial continua.
+        De lo contrario, se devuelve una respuesta de error 403.
+
+        :param request: La solicitud HTTP que contiene los datos de actualización.
+        :param args: Argumentos adicionales.
+        :param kwargs: Argumentos adicionales con nombre.
+        :return: Una respuesta HTTP con el resultado de la operación de actualización.
+        """
+        review = self.get_object()
+        permission_check = self._check_group(request, review)
+
+        if isinstance(permission_check, Response):
+            return permission_check  # Retorna la respuesta 403 si no tiene permisos
+
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        review = self.get_object()
+        permission_check = self._check_group(request, review)
+
+        if isinstance(permission_check, Response):
+            return permission_check  # Retorna 403 si no tiene permisos
+
+        return super().destroy(request, *args, **kwargs)
 
 
 # region Reviews
